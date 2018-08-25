27be90ec76e56c1d656409751dcaec2d0b20bc43
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 11:31:28 +0530

    06: array spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,7 @@
-// function sumAll(arr: any) {
-//     return arr.reduce((prev: any, next: any) => prev + next);
-// }
+const toppings = ['bacon', 'chilli'];
 
-// const sum = sumAll([1,2,3,4,5,6,7,8,9,10]); 
+const newToppings = ['pepperoni'];
 
-// console.log(sum);
+const allToppings = [...toppings, ...newToppings];
 
-function sumAll(message: any, ...arr: any[]) {
-    console.log(message);
-    console.log(arguments);
-    return arr.reduce((prev: any, next: any) => prev + next);
-}
-
-const sum = sumAll('hello',1,2,3,4,5,6,7,8,9,10); 
-
-console.log(sum);
+console.log(allToppings);

70e62ae7614a5bae90acfc75b7167285ebe6d52f
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 19:23:41 +0530

    35: abstract class

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-class Sizes {
+abstract class Sizes {
     constructor(public sizes: string[]) {}
 
     set availableSizes(sizes: string[]) {
@@ -22,20 +22,11 @@ class Pizza extends Sizes {
     }
 }
 
-// function Pizza(name: string) {
-//     this.name = name;
-//     this.toppings = [];
-// }
-
-// Pizza.prototype.addTopping = function addTopping(topping: string) {
-//     this.toppings.push(topping);
-// };
-
 const pizza = new Pizza('Pepperoni', ['medium', 'large']);
-console.log(pizza.availableSizes);
 pizza.addTopping('pepperoni');
+
+console.log(pizza.availableSizes);
 console.log(pizza.name);
 console.log(pizza);
 
-// readonly properties can only be initialized at the declaration, they can't be
-// changed after that.
+// you cannot instantiate an abstract class

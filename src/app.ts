8a755da049532c6754889f46d062d3431a432eca
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 15:03:05 +0530

    24: type assertions

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
-type Size = 'small' | 'medium' | 'large';
-type Callback = (size: Size) => void;
+// type Pizza = {name: string, toppings: number};
 
-let pizzaSize: Size = 'small';
+// const pizza: Pizza = {name: 'Blazing Inferno', toppings: 5};
 
-const selectSize: Callback = x => {
-    pizzaSize = x;
-};
+// const serialize = JSON.stringify(pizza);
 
-selectSize('small');
+// function getNameFromJSON(obj: string) {
+//     return (<Pizza>JSON.parse(obj)).name;
+// }
+
+// getNameFromJSON(serialize);
+
+type Pizza = {name: string, toppings: number};
+
+const pizza: Pizza = {name: 'Blazing Inferno', toppings: 5};
+
+const serialize = JSON.stringify(pizza);
+
+function getNameFromJSON(obj: string) {
+    return (JSON.parse(obj) as Pizza).name;
+}
+
+getNameFromJSON(serialize);

421d270ca17e0be511d32802870719e575c32815
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 19:03:31 +0530

    33: setters and getters (accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+class Sizes {
+    constructor(public sizes: string[]) {}
+
+    set availableSizes(sizes: string[]) {
+        this.sizes = sizes;
+    }
+
+    get availableSizes() {
+        return this.sizes;
+    }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+
+// invoke getter
+console.log(sizes.availableSizes);
+
+// invoke setter
+sizes.availableSizes = ['medium', 'large'];
+console.log(sizes.availableSizes);
+
+// setters and getters are always public
+
 // a class is syntactical sugar for creating our own constructor functions
 // and prototypal inheritance
 
